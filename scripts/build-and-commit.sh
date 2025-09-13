d06ab285c6756a89829a0a7591849ddf1b6f9494
Chiranthcs6 <chiranthcs5@gmail.com>
2025-09-13 09:18:20 +0530

    updated the css folder name to styles

--- scripts/build-and-commit.sh
+++ scripts/build-and-commit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-npx tailwindcss -i ./src/input.css -o ./dist/style.css
-git add dist/style.css
+npx tailwindcss -i ./src/input.css -o ./styles/style.css
+git add styles/style.css
 git commit -m "Update built CSS"
 git push
 echo "✅ CSS updated and pushed to GitHub!"
